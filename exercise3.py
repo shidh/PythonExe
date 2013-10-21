f6960198e2fc7ab37c1b6ee7d59e612389e6b4dd
Denghui Shi <shi@253-79.informatik.tu-muenchen.de>
2013-10-21 15:38:06 +0200

    add exercise1-10

---
+++ exercise3.py
@@ -0,0 +1,35 @@
+print "I will now count my chickens:"
+
+print "Hens", 25.00 + 30.00 / 6.00
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+#"X divided by Y with J remaining, J is the result of %"
+print "modulus of 99 by 7 is?", 99%7
+
+# (divide) round down
+print "7.0 / 4.0 is? ", 7.0 / 4.0
+print "7 / 4 is ?" , 7/4
+print "1 / 3 is ?" , 1/3
+print "1 / 3.0 is ?" , 1/3.0
+
+
+print "Hey %s there." % "you are"
---
+++ exercise9.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#-*- encoding: utf-8 -*-
+
+print 
+'''
+comments
+and
+more comments
+
+'''
+
+days = "Mon. Tue. Wed. Tur. Fri. Sat. Sun. "
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are days: ", days
+print "Here are months: ", months
+
+print "I am 6'2\" tall."  # escape double-quote inside string
+print 'I am 6\'2" tall.'  # escape single-quote inside string
